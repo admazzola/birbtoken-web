4bc21108566a389289c45385fa7c17bcc6e71d45
andy <admazzola@gmail.com>
2021-06-17 04:46:30 -0400

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,170 @@
+<template>
+
+<div>
+
+   <div class="section  bg-gray-200  border-b-4 border-black px-0 lg:px-1">
+
+     <div class=" ">
+        <Navbar 
+        v-bind:web3Plug="web3Plug"
+        
+       />
+     </div>
+
+
+   </div>
+
+  
+
+   <div class="section  border-b-2 border-black" style="background:#1d1d1d;">
+     <div class=" ">
+       <div class=" ">
+
+       </div>
+       <div class="  flex lg:flex-row flex-col  ">
+ 
+         <div class="  md:w-1/2  w-full mt-8 py-8  px-1  text-center">
+
+            <div class="hidden text-white text-xl my-4">  Neutral grass-roots organization.  </div> 
+
+        <div>
+            <router-link to="/stake"  class="select-none no-underline bg-blue-700 mb-16 p-2 inline-block rounded hover:bg-blue-900 border-gray-800 border-2 cursor-pointer text-blue-100" style=" text-shadow: 1px 1px #222;"> Join the Guild   </router-link> 
+        </div>
+      
+            <img src="@/assets/images/hero-dark2.png" class="pl-4" style="margin:0 auto;" />
+
+         </div>
+         <div class="   md:w-1/2  w-full  text-center ">
+           
+            
+           <FrontPageMedia />
+
+
+         </div>
+       </div>
+     </div>
+   </div>
+
+
+    <div class="section  text-white  border-b-2 border-black " style="background:#222;">
+     <div class="w-container  ">
+
+         
+
+          <div class=" w-2/3  mt-8 py-8" style="margin: 0 auto;">
+           
+                <div class="text-2xl text-center"> How It Works </div>
+
+                <ul class=" "> 
+                    <li class="my-4"> 1. Stake 0xBTC in the Miners Guild Contract Pool </li>
+                    <li class="my-4" > 2. Neutral Dapps/Contracts donate their fees [in execution flow] to the Pool to earn Community Goodwill </li>
+                    <li class="my-4" > 3. Unstake at any time to withdraw <span class="text-purple-300"> your original deposit + accrued donations </span>  </li>
+               </ul>
+
+                <div class="text-lg text-center my-16 hidden "> TLDR: This is a funnel for donations from community-driven dapps. </div>
+
+
+                <p class="text-gray-500 bg-gray-800 p-4"> 🛈 Smart contracts that pay fees into the MinersGuild contract, during execution flow, are more likely to be promoted, supported, and used by the community than ones that do not. </p>
+
+         </div>
+
+        
+         
+
+
+     </div>
+   </div>
+
+
+
+    
+  <Footer/>
+
+</div>
+</template>
+
+
+<script>
+
+
+
+import Web3Plug from '../js/web3-plug.js'  
+
+ 
+import FrontPageMedia from './components/FrontPageMedia.vue';
+ 
+import Navbar from './components/Navbar.vue';
+ 
+import Footer from './components/Footer.vue';
+import TabsBar from './components/TabsBar.vue';
+  
+import StarflaskAPIHelper from '../js/starflask-api-helper.js';
+import FrontendHelper from '../js/frontend-helper.js';
+
+
+export default {
+  name: 'Home',
+  props: [],
+  components: {Navbar, Footer, TabsBar, FrontPageMedia },
+  data() {
+    return {
+      web3Plug: new Web3Plug() , 
+      activePanelId: null, 
+       
+
+      
+    }
+  },
+
+  created(){
+
+ 
+    this.web3Plug.getPlugEventEmitter().on('stateChanged', function(connectionState) {
+        console.log('stateChanged',connectionState);
+         
+        this.activeAccountAddress = connectionState.activeAccountAddress
+        this.activeNetworkId = connectionState.activeNetworkId 
+         
+      }.bind(this));
+   this.web3Plug.getPlugEventEmitter().on('error', function(errormessage) {
+        console.error('error',errormessage);
+         
+        this.web3error = errormessage
+       
+      }.bind(this));
+
+      this.web3Plug.reconnectWeb()
+   
+       
+
+  },
+  mounted: function () {
+         
+    
+  }, 
+  methods: {
+          setActivePanel(panelId){
+              if(panelId == this.activePanelId){
+                this.activePanelId = null;
+                return 
+              }
+               this.activePanelId = panelId ;
+          },
+          onTabSelect(tabname){
+            console.log(tabname)
+
+              this.selectedTab = tabname.toLowerCase()
+
+
+          },
+
+          getRouteTo(dest){
+            return FrontendHelper.getRouteTo(dest)
+          }
+
+       
+ 
+
+  }
+}
+</script>
---
+++ src/views/components/Footer.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="section py-4 text-white h-full" style="background: #1d1e26; min-height: 250px;">
+    <div class=" h-full ">
+      <div class=" flex lg:flex-row h-full">
+         <div class="lg:w-1/4 ">
+         </div>
+        <div class="w-1/2 lg:w-1/4 flex flex-col">
+          <h3 class=" text-lg font-bold ">Documentation</h3>
+
+          <a href="https://etherscan.io/address/0x167152a46e8616d4a6892a6afd8e52f060151c70" target="_blank" class="no-underline inline-block text-sm text-gray-200 hover:text-purple-300 ">Guild Contract</a>
+           <a href="https://etherscan.io/address/0x657223e3fdf539d92c40664db340097d5d6bd9f5" target="_blank" class="no-underline inline-block text-sm text-gray-200 hover:text-purple-300">Reserve Token</a>
+              
+              
+          </div>
+        <div class="w-1/2 lg:w-1/4 flex flex-col">
+          <h3 class=" text-lg font-bold ">Community &amp; Social</h3>
+           <a href="https://0xbitcoin.org" target="_blank" class="no-underline text-sm text-gray-200 hover:text-purple-300 inline-block">0xBTC Website</a>
+         
+         
+         </div>
+
+
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+
+
+export default {
+  name: 'Footer',
+  props: [],
+  data() {
+    return {
+
+    }
+  },
+  created(){
+
+  },
+  methods: {
+
+  }
+}
+</script>
